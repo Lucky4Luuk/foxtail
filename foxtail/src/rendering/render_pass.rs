63df9d4e97fa2acf59206ca4b885a081573aa4f2
Luuk van Oijen <lazyluuk.channel@gmail.com>
2023-08-03 22:52:26 +0200

    Update render_pass.rs

--- foxtail/src/rendering/render_pass.rs
+++ foxtail/src/rendering/render_pass.rs
@@ -15,7 +15,9 @@ pub struct Framebuffer {
 impl super::Drawable for Framebuffer {
     fn draw(&self) -> Result<(), super::RenderError> {
         if self.shader_bound.load(Ordering::Acquire) {
+            self.bind_tex();
             self.mesh.draw()?;
+            self.unbind_tex();
         } else {
             self.default_fb_shader.while_bound(|_| {
                 self.bind_tex();
